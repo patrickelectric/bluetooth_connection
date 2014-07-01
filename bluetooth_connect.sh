98f03e9a7471f4c3f922f720feed79fe9169abbb
Patrick <patrickelectric@gmail.com>
2014-07-01 19:34:50 -0300

    modificado para receber argumentos

--- bluetooth_connect.sh
+++ bluetooth_connect.sh
@@ -1,8 +1,15 @@
-echo "scan bluetooth 00:13:01:08:15:83.."
+#argumento 1 = $1
+echo "scaneando bluetooths"
 hcitool scan
-echo "connection with  bluetooth.."
-sudo rfcomm connect 0 00:13:01:08:15:83 1
-echo "checking.."
-sdptool browse 00:13:01:08:15:83
+
+if [ $# -lt 1 ]; then
+	echo " "
+	echo "The program need de mac address of the bt"
+	echo "sh bluetooth_connect.sh 00:00:00:00:00:00"
+	exit 1
+fi
+
+echo "connectando bluetooth"
+sudo rfcomm connect 0 $1 1
 
 
