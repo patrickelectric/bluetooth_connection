afb52d8e5d0b14821b49229d947edfb0b31e1dfc
Patrick J.P <patrickelectric@gmail.com>
2014-12-08 01:25:50 -0200

    melhorando código para ser mais rapido

--- bluetooth_connect.sh
+++ bluetooth_connect.sh
@@ -1,8 +1,7 @@
 #argumento 1 = $1
-echo "scan for bluetooths"
-hcitool scan
-
 if [ $# -lt 1 ]; then
+	echo "scan for bluetooths"
+	hcitool scan
 	echo " "
 	echo "The program need the mac address of the bluetooth"
 	echo "sh bluetooth_connect.sh 00:00:00:00:00:00"
